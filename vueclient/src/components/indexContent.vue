5be9a942e0b5ccc5332592461a258580cc8d5714
TangSong <3016218065@tju.edu.cn>
2018-12-15 00:01:07 +0800

    add route service

---
+++ vueclient/src/components/indexContent.vue
@@ -0,0 +1,71 @@
+<!--<template>-->
+  <!--<div>-->
+    <!--&lt;!&ndash;<iframe v-show="iframeState" id="show-iframe"  frameborder=0 name="showHere" scrolling=auto src=""></iframe>&ndash;&gt;-->
+    <!--<iframe src="../../static/rm100/chinamap.html" width="1200" height="300" frameborder="0" scrolling="auto"style="position:absolute;top: -300px;left: 0px;"></iframe>-->
+  <!--</div>>-->
+<!--</template>-->
+<template>
+  <div class="accept-container">
+    <div class="go-back" v-show="goBackState" @click="goBack">GoBack</div>
+    <ul>
+      <li v-for="item in webAddress">
+        <a :href="item.link" target="showHere" @click="showIframe">{{item.name}}</a>
+      </li>
+    </ul>
+    <iframe v-show="iframeState" id="show-iframe"  frameborder=0 name="showHere" scrolling=auto src=""></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        iframeState:false,
+        goBackState:false,
+        webAddress: [
+          {
+            name:'segmentFault',
+            link:'https://segmentfault.com/a/1190000004502619'
+          },
+          {
+            name:'博客',
+            link:'http://vuex.vuejs.org/'
+          },
+          {
+            name:'特效',
+            link:'http://www.yyyweb.com/377.html'
+          },{
+            name:'地图',
+            link:'../../static/rm100/chinamap.html'
+          },{
+            name:'本文教程',
+            link:'https://www.cnblogs.com/rlann/p/7457601.html'
+          }
+        ]
+      }
+    },
+    mounted(){
+      const oIframe = document.getElementById('show-iframe');
+      const deviceWidth = document.documentElement.clientWidth;
+      const deviceHeight = document.documentElement.clientHeight;
+      oIframe.style.width = deviceWidth + 'px';
+      oIframe.style.height = deviceHeight + 'px';
+    },
+    methods:{
+      goBack(){
+        this.goBackState = false;
+        this.iframeState = false;
+      },
+      showIframe(){
+        this.goBackState = true;
+        this.iframeState = true;
+      }
+    }
+  }
+
+</script>
+<style scoped>
+
+
+</style>
---
+++ vueclient/src/components/route.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class = "wrapper">
+      <img class = "content-pic" src="../assets/images/desert.png"/>
+      <div class = "text-wrapper">
+        <div class = "route_title" >旅行项目名称</div>
+        <div class = "route_destination" >destination</div>
+        <div class = "route_date" >date</div>
+        <div class = "route_cost" >Cost</div>
+        <div class = "description" >description</div>
+      </div>
+      <button id = "purchase_btn" class = "btn" @click = "purchase()">订购项目</button>
+      <button @click = "getRouteInfo()"></button>
+    </div>
+
+</template>
+
+<script>
+  export default{
+    methods:{
+      purchase(){
+        alert("订购成功");
+      },
+      getRouteInfo(name){
+        var _this = this;
+        this.$reqs.post('/users/OrderList',{
+          name:"北京"
+        }).then(function(result){
+          //成功
+          _this.listData = result.data.data;
+          _this.pageInfo.allpage = Math.ceil( result.data.total/5 );
+        }).catch(function (error) {
+          //失败
+          console.log(error)
+        });
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .wrapper{
+    margin:20px 0px 0px 0px;
+    border-radius: 4px;
+    padding: 20px 15px;
+    background: #fff;
+    height: 420px;
+  }
+  .content-pic {
+    margin: 40px 140px 40px 40px;
+    float: left;
+    width: 330px;
+    height: 330px;
+    border-radius: 5%;
+  }
+
+  .text-wrapper {
+    margin-left: 450px;
+  }
+
+  .route_title {
+    margin-top:20px;
+    font-size: 30px;
+  }
+
+  .route_destination {
+    margin-top: 30px;
+    font-size:15px;
+    color: #CFCFCF;
+  }
+
+  .route_date {
+    font-size:15px;
+    color: #CFCFCF;
+  }
+
+  .route_cost{
+    margin-top: 45px;
+    font-size:20px;
+  }
+
+  .description{
+    margin-top: 50px;
+    font-size:22px;
+  }
+
+  #purchase_btn {
+    float:right;
+    margin-top: 90px;
+    height:45px;
+    width:150px;
+    font-size:18px;
+  }
+
+</style>
